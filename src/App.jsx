287505ccee14c5841b27edfd6fc67197e83cad35
Robert King <robert.o.king@hotmail.co.uk>
2022-01-13 16:02:39 +0000

    feat: useEventListener hook used to allow keyboard input

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,42 @@ import './App.css';
 
 import InputButton from './components/InputButton.jsx';
 
+import { useEventListener } from './hooks/useEventListener.js';
+
 const App = () => {
-  const [currentInputString, setCurrentInputString] = useState('');
-  const inputs = [1, 2, 3, 4, '+', '-', '*', '/', '(', ')'];
+  const [currentInputStr, setCurrentInputStr] = useState('');
+  const [currentInputVal, setCurrentInputVal] = useState(0);
+  const inputs = ['1', '2', '3', '4', '+', '-', '*', '/', '(', ')'];
+
+  const updateInputVal = (newInputStr) => {
+    const whitelistedStr = newInputStr.replace(/[^0-9\(\)\+\-\*\/\.]/g, "");
+    const newInputVal = eval(whitelistedStr);
+    setCurrentInputVal(newInputVal);
+  };
 
   const inputHandler = (input) => {
-    setCurrentInputString(currentInputString + input);
+    const newInputStr = currentInputStr + input;
+    setCurrentInputStr(newInputStr);
+    updateInputVal(newInputStr);
   };
+
+  useEventListener('keydown', (e) => {
+    e.preventDefault();
+    if (inputs.includes(e.key)) { inputHandler(e.key); }
+  });
 
   return (
     <div className="App">
       <h1>Forty-Two</h1>
       <div className="grid">
         <div className="output span-four">
-          <div className="output-calculation">{currentInputString}</div>
-          <div className="output-value"></div>
+          <div className="output-calculation">{currentInputStr}</div>
+          <div className="output-value">{currentInputVal}</div>
         </div>
         {
-          inputs.map((input, id) => { return <InputButton key={id} input={input} clickHandler={inputHandler} />; })
+          inputs.map((input, id) => {
+            return <InputButton key={id} input={input} clickHandler={inputHandler} />;
+          })
         }
         <div className="button span-two" role="button" tabIndex="0">Back</div>
         <div className="button span-four" role="button" tabIndex="0">=</div>
