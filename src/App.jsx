9c22b4c4a9b9bd311b92425ec9efeadc27d63d51
Robert King <robert.o.king@hotmail.co.uk>
2022-01-19 00:48:18 +0000

    feat: Displaying answers on page

--- src/App.jsx
+++ src/App.jsx
@@ -103,6 +103,7 @@ const App = () => {
         <div className="button span-two" role="button" tabIndex="0" onClick={backspaceHandler}>Back</div>
         <div className="button span-four" role="button" tabIndex="0" onClick={enterHandler}>=</div>
       </div>
+      <div>You've found: {answers.join(', ')}</div>
     </div>
   );
 }
