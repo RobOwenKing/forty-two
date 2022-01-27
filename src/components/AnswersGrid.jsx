456d92e6010e3de63e2cc50219cbbe4ea718d920
Robert King <robert.o.king@hotmail.co.uk>
2022-01-27 17:31:55 +0000

    feat: Better styling for <AnswersGrid>

--- src/components/AnswersGrid.jsx
+++ src/components/AnswersGrid.jsx
@@ -1,10 +1,23 @@
 const AnswersGrid = ({ answers }) => {
+  const fontSize = (answer) => {
+    if (!answer) { return 'inherit'; }
+    if (answer.length < 9) { return 'inherit'; }
+
+    return (80 / answer.length) * 1.2;
+  }
+
   return (
-    <div class="answers-grid">
+    <div className="answers-grid">
       {
         answers.map((answer, id) => {
           return (
-            <div key={id} className={answer ? "solved" : "not-solved"}>{answer ? answer : id+1}</div>
+            <div
+                key={id}
+                className={answer ? "solved" : "not-solved"}
+                style={{fontSize: fontSize(answer)}}
+            >
+              {answer ? answer : id+1}
+            </div>
           )
         })
       }
