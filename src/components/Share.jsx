e503c8f9ec55c737b183f114ae01a10320c67531
Robert King <robert.o.king@hotmail.co.uk>
2022-02-08 20:41:25 +0000

    feat: 5 other <Share> buttons

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -18,7 +18,7 @@ import {
 
 const Share = ({ answers }) => {
   const title = "Twenty-Eight, Your Daily Numbers Game";
-  const url = window.location.href;
+  const url = "http://twenty-eight.robowenking.com/";
   const iconSize = 48;
 
   return (
@@ -27,6 +27,21 @@ const Share = ({ answers }) => {
       <TwitterShareButton url={url} title={title}>
         <TwitterIcon size={iconSize} round />
       </TwitterShareButton>
+      <FacebookShareButton url={url} title={title}>
+        <FacebookIcon size={iconSize} round />
+      </FacebookShareButton>
+      <EmailShareButton url={url} title={title}>
+        <EmailIcon size={iconSize} round />
+      </EmailShareButton>
+      <PocketShareButton url={url} title={title}>
+        <PocketIcon size={iconSize} round />
+      </PocketShareButton>
+      <TelegramShareButton url={url} title={title}>
+        <TelegramIcon size={iconSize} round />
+      </TelegramShareButton>
+      <WhatsappShareButton url={url} title={title}>
+        <WhatsappIcon size={iconSize} round />
+      </WhatsappShareButton>
     </div>
   );
 }
