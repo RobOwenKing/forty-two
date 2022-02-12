1bb8deb1f10ec51a6253468e8e1244d91a8f6364
Robert King <robert.o.king@hotmail.co.uk>
2022-02-12 15:08:29 +0000

    feat: handleInputElementInput() can handle digits, including 10, 11, 12

--- src/helpers/handleInputElementInput.js
+++ src/helpers/handleInputElementInput.js
@@ -4,7 +4,23 @@ export const handleInputElementInput = (str, operations) => {
 
   for (let i = 0; i < str.length; i += 1) {
     const char = str[i];
-    if (operations.includes(char)) { newCurrentInputArr.push(char); }
+
+    if (operations.includes(char)) {
+      newCurrentInputArr.push(char);
+    } else if (['0', '2', '3', '4', '5', '6', '7', '8', '9'].includes(char)) {
+      newCurrentInputArr.push(char);
+      newDigitsUsed.push(char);
+    } else if (char === '1') {
+      if (['0', '1', '2'].includes(str[i+1])) {
+        const digits = str.slice(i, i+2);
+        newCurrentInputArr.push(digits);
+        newDigitsUsed.push(digits);
+        i += 1
+      } else {
+        newCurrentInputArr.push(char);
+        newDigitsUsed.push(char);
+      }
+    }
   }
 
   return newCurrentInputArr;
