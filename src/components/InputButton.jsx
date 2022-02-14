463890a3751683159a51810a015f473c6d0c3c25
Robert King <robert.o.king@hotmail.co.uk>
2022-02-14 20:16:32 +0000

    refactor: Comments in <InputButton>

--- src/components/InputButton.jsx
+++ src/components/InputButton.jsx
@@ -1,3 +1,8 @@
+/**
+  * @param {number|string} input - Input option to be represented by the button
+  * @param {function} clickHandler - To be called onClick
+  * @param {string} [classes] - Used directly as className to allow custom styling
+*/
 const InputButton = ({ input, clickHandler, classes = '' }) => {
   return (
     <div
