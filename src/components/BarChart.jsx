1f001a9f936f1d6068bed20ce67dafc58a3d09c6
Robert King <robert.o.king@hotmail.co.uk>
2022-02-14 19:56:51 +0000

    refactor: Comments in <BarChart>

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,4 +1,11 @@
+/**
+  * @param {array.number} data - The values for the sizes of the bars
+  * @param {array.boolean} highlights - Same length as data. Defines colouring of the bars: true = orange; false = blue.
+  * @param {string} divClass - Used directly as className to allow custom styling
+  * @param {array.string} [labels] - Same length as data. Label to describe what each bar represents.
+*/
 const BarChart = ({ data, highlights, divClass, labels = null }) => {
+  /** Used to scale bars */
   const max = Math.max(...data);
 
   return (
