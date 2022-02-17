cbd29ebe653e2a1339d37bed3929f9e2f48bb265
Robert King <robert.o.king@hotmail.co.uk>
2022-02-17 20:38:42 +0000

    feat: clickHandler() for every <span> in <EquationInput> to set cursor to correct place

--- src/components/EquationInput.jsx
+++ src/components/EquationInput.jsx
@@ -1,20 +1,30 @@
 /**
   * @param {number} cursorPos - Where in inputArr the cursor should be
   * @param {function} setCursorPos - Function to change the value of cursorPos
-  * * @param {array.<string>} inputArr - The array of digits and operations entered by the player
+  * @param {array.<string>} inputArr - The array of digits and operations entered by the player
 */
 const EquationInput = ({ cursorPos, setCursorPos, inputArr }) => {
+  const clickHandler = (e, id) => {
+    const isOnLeftHalf = e.nativeEvent.offsetX < e.target.offsetLeft + 0.5*e.target.offsetWidth
+
+    if (isOnLeftHalf) {
+      setCursorPos(id);
+    } else {
+      setCursorPos(id + 1);
+    }
+  };
+
   return (
     <div>
       {inputArr.slice(0, cursorPos).map((entry, id) => {
         return (
-          <span key={id}>{entry}</span>
+          <span key={id} onClick={(e) => clickHandler(e, id)}>{entry}</span>
         )
       })}
       <span>c</span>
       {inputArr.slice(cursorPos).map((entry, id) => {
         return (
-          <span key={id}>{entry}</span>
+          <span key={id} onClick={(e) => clickHandler(e, id)}>{entry}</span>
         )
       })}
     </div>
