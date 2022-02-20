b7b5998bd3cc520435e581d513a314856cbe1801
Robert King <robert.o.king@hotmail.co.uk>
2022-02-20 01:06:36 +0000

    refactor: Remove import of useRef as no longer necessary

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { evaluate } from 'mathjs';
 
 import DigitButton from './DigitButton.jsx';
