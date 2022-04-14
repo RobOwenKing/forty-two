988686bbbe9ac7f0bc598f725ba07f37baed3d2a
Robert King <robert.o.king@hotmail.co.uk>
2022-04-14 22:11:59 +0100

    feat: Add convertStoredHistory()

--- src/helpers/storeNewHistory.js
+++ src/helpers/storeNewHistory.js
@@ -12,6 +12,29 @@ const updateStoredNewHistory = (date, score, max, storedNewHistory = '{}') => {
 };
 
 /**
+  * Updates localStorage history (given it exists) with new score
+  * @param {string} date - String representing the current date, used to seed the random number generator
+  * @param {number} score - User's current score for the day
+  * @param {boolean} max - Whether the player has reached the day's max score or not
+  * @param {string} storedHistory - Unparsed contents of localStorage.history
+*/
+const convertStoredHistory = (storedHistory, date, score, max) => {
+  const newHistory = {};
+
+  const oldHistory = JSON.parse(storedHistory);
+  let start = false;
+  let d = new Date();
+  while (d.toDateString() !== oldHistory['lastPlayed']) { d.setDate(d.getDate() - 1); }
+  while (oldHistory['scores'].length > 0) {
+    const e = oldHistory['scores'].pop();
+    if (e > 0) { newHistory[d.toDateString()] = e; }
+    d.setDate(d.getDate() - 1);
+  }
+
+  updateStoredNewHistory(date, score, max, JSON.stringify(newHistory));
+};
+
+/**
   * Handles creating and updating the player's score history in localStorage
   * @param {string} date - String representing the current date, used to seed the random number generator
   * @param {number} score - User's current score for the day
