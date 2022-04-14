94006972b47756f027574cafc36b4b8e3b61ea0b
Robert King <robert.o.king@hotmail.co.uk>
2022-04-14 20:42:31 +0100

    feat: Add updateStoredNewHistory(), and refactor storeNewHistory() to use it twice

--- src/helpers/storeNewHistory.js
+++ src/helpers/storeNewHistory.js
@@ -1,4 +1,17 @@
 /**
+  * Updates localStorage history (given it exists) with new score
+  * @param {string} date - String representing the current date, used to seed the random number generator
+  * @param {number} score - User's current score for the day
+  * @param {boolean} max - Whether the player has reached the day's max score or not
+  * @param {string} storedHistory - Unparsed contents of localStorage.history
+*/
+const updateStoredNewHistory = (date, score, max, storedNewHistory = '{}') => {
+  const newHistory = JSON.parse(storedNewHistory);
+  newHistory[date] = { 'score': score, 'max': max };
+  localStorage.setItem('newHistory', JSON.stringify(newHistory));
+};
+
+/**
   * Handles creating and updating the player's score history in localStorage
   * @param {string} date - String representing the current date, used to seed the random number generator
   * @param {number} score - User's current score for the day
@@ -9,60 +22,14 @@ export const storeNewHistory = (date, score, max) => {
   const storedHistory = localStorage.getItem('history');       // Old format
 
   if (storedNewHistory) {
-    updateStoredNewHistory(storedNewHistory, date, score, max);
+    updateStoredNewHistory(date, score, max, storedNewHistory);
   } else {
     if (score <= 0) { return; } // Streak should only start with a score > 0
 
     if (storedHistory) {
       convertStoredHistory(storedHistory, date, score, max);
     } else {
-      createStoredNewHistory(date, score, max);
+      updateStoredNewHistory(date, score, max);
     }
   }
-};
-
-
-
-
-
-
-
-
-
-
-/**
-  * Updates localStorage history (given it exists) with new score
-  * @param {string} storedHistory - Unparsed contents of localStorage.history
-  * @param {string} date - String representing the current date, used to seed the random number generator
-  * @param {number} score - User's current score for the day
-*/
-const updateStoredHistory = (storedHistory, date, score) => {
-  const parsedStoredHistory = JSON.parse(storedHistory);
-
-  if (parsedStoredHistory['lastPlayed'] === date) {
-    // If the user has already played today, update last entry in scores array
-    const lengthOfScores = parsedStoredHistory['scores'].length;
-    parsedStoredHistory['scores'][lengthOfScores - 1] = score;
-  } else {
-    // If the user hasn't played today, add day's score to end of array and update date
-    parsedStoredHistory['scores'] = fillScores(parsedStoredHistory);
-    parsedStoredHistory['lastPlayed'] = date;
-    parsedStoredHistory['scores'].push(score);
-  }
-
-  localStorage.setItem('history', JSON.stringify(parsedStoredHistory));
-};
-
-/**
-  * Creates history object in localStorage if new player
-  * @param {string} date - String representing the current date, used to seed the random number generator
-  * @param {number} score - User's current score for the day
-*/
-const createStoredHistory = (date, score) => {
-  const storableHistory = JSON.stringify({
-    firstPlayed: new Date(),
-    lastPlayed: date,
-    scores: [score]
-  });
-  localStorage.setItem('history', storableHistory);
 };
