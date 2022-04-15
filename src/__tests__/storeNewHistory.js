b75a1b2d25fe96551863f60bba81bacf173abfe4
Robert King <robert.o.king@hotmail.co.uk>
2022-04-15 17:53:38 +0100

    test: First simple test case for convertStoredHistory()

--- src/__tests__/storeNewHistory.js
+++ src/__tests__/storeNewHistory.js
@@ -1,7 +1,19 @@
 import { convertStoredHistory } from '../helpers/storeNewHistory.js';
 
 describe('convertStoredHistory()', () => {
-  /* it('should work with a lastPlayed of today') */
+  it('should work with a lastPlayed of today', () => {
+    const oldHistory = {
+      'lastPlayed': new Date().toDateString(),
+      'scores': [1, 2, 3]
+    };
+    const newHistory = convertStoredHistory(oldHistory);
+    let d = new Date();
+    expect(newHistory[d.toDateString()]['s']).toBe(3);
+    d.setDate(d.getDate() - 1);
+    expect(newHistory[d.toDateString()]['s']).toBe(2);
+    d.setDate(d.getDate() - 1);
+    expect(newHistory[d.toDateString()]['s']).toBe(1);
+  });
   /* it('should work with a lastPlayed in the past') */
   /* it('should work over month boundaries') */
   /* it('should ignore days when 0 was scored') */
