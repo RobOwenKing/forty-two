4abdc2acc7838fa834801cf704db6a828ac4e871
Robert King <robert.o.king@hotmail.co.uk>
2022-04-15 18:07:11 +0100

    test: Cover convertStoredHistory() when lastPlayed is not today

--- src/__tests__/storeNewHistory.js
+++ src/__tests__/storeNewHistory.js
@@ -14,7 +14,17 @@ describe('convertStoredHistory()', () => {
     d.setDate(d.getDate() - 1);
     expect(newHistory[d.toDateString()]['s']).toBe(1);
   });
-  /* it('should work with a lastPlayed in the past') */
+  it('should work with a lastPlayed in the past', () => {
+    const oldHistory = {
+      'lastPlayed': "Mon Apr 11 2022",
+      'scores': [4, 5, 6]
+    };
+    const newHistory = convertStoredHistory(oldHistory);
+    expect(newHistory["Mon Apr 11 2022"]['s']).toBe(6);
+    expect(newHistory["Sun Apr 10 2022"]['s']).toBe(5);
+    expect(newHistory["Sat Apr 09 2022"]['s']).toBe(4);
+    expect(Object.entries(newHistory).length).toBe(3);
+  });
   /* it('should work over month boundaries') */
   /* it('should ignore days when 0 was scored') */
 })
