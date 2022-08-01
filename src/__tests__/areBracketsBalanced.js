d66153a3c206da5522aae35e60d1dee7ec28b058
Robert King <robert.o.king@hotmail.co.uk>
2022-08-01 16:06:59 +0100

    test: Add more tests of areBracketsBalanced()

--- src/__tests__/areBracketsBalanced.js
+++ src/__tests__/areBracketsBalanced.js
@@ -4,4 +4,28 @@ describe('areBracketsBalanced()', () => {
   it('should return true in the null case', () => {
     expect(areBracketsBalanced("2 + 2 = 4")).toBeTruthy();
   });
+  it('should return true in the simplest case', () => {
+    expect(areBracketsBalanced("(2 + 2) = 4")).toBeTruthy();
+  });
+  it('should return true with valid simple nesting', () => {
+    expect(areBracketsBalanced("((2 + 2) + 2) = 6")).toBeTruthy();
+  });
+  it('should return true with valid complex nesting', () => {
+    expect(areBracketsBalanced("((2 + 2) + ((2 + 2) + 2)) = 10")).toBeTruthy();
+  });
+  it('should return false with invalid order of brackets', () => {
+    expect(areBracketsBalanced("())(()")).toBeFalsy();
+  });
+  it('should return false when not all pairs close', () => {
+    expect(areBracketsBalanced("((2 + 2) = 4")).toBeFalsy();
+  });
+  it('should return false when too many pairs close', () => {
+    expect(areBracketsBalanced("(2 + 2)) = 4")).toBeFalsy();
+  });
+  it('should return false when none open', () => {
+    expect(areBracketsBalanced("2 + 2) = 4")).toBeFalsy();
+  });
+  it('should return false when none close', () => {
+    expect(areBracketsBalanced("(2 + 2 = 4")).toBeFalsy();
+  });
 })
