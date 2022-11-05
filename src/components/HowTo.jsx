0e58159dddbb7cabd60a957891c6a55c5cb07f65
Robert King <robert.o.king@hotmail.co.uk>
2022-11-05 15:23:50 +0000

    refactor: Delete old classes from <HowTo>

--- src/components/HowTo.jsx
+++ src/components/HowTo.jsx
@@ -6,7 +6,7 @@ const HowTo = () => {
         This is a game about a strange calculator. A calculator with only four
         digits, which change every day.
       </p>
-      <div className="how-to-grid grid has-one-row">
+      <div className="grid has-one-row">
         <div className="grid__cell flex-center">6</div>
         <div className="grid__cell flex-center used">2</div>
         <div className="grid__cell flex-center">11</div>
@@ -16,7 +16,7 @@ const HowTo = () => {
         Each digit button must be used exactly once in every calculation. You
         can use the other symbols as many or as few times as you wish.
       </p>
-      <div className="how-to-calc grid has-output-only">
+      <div className="grid has-output-only">
         <div className="grid__cell is-span-four">
           <div className="calculator__equation">
             (4-2)*11+6<span className="caret"></span>
@@ -31,7 +31,7 @@ const HowTo = () => {
       <p className="text-page__p">
         Press equals when you've found a new number to score points.
       </p>
-      <div className="how-to-grid grid has-one-row">
+      <div className="grid has-one-row">
         <div className="grid__cell flex-center is-span-four calculator__equals">
           =
         </div>
